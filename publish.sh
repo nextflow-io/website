13ecc1cae4cb0d7e8e9a935908f4a7764b013f64
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2015-04-09 10:44:47 +0200

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-s3cmd sync output/* s3://www.nextflow.io \
- -rr \
- --acl-public \
- --no-mime-magic \
- --access_key=$NXF_AWS_ACCESS \
- --secret_key=$NXF_AWS_SECRET \
+
+AWS_ACCESS_KEY_ID=$NXF_AWS_ACCESS
+AWS_SECRET_ACCESS_KEY=$NXF_AWS_SECRET
+ 
+aws s3 sync output/ s3://www.nextflow.io \
+ --storage-class REDUCED_REDUNDANCY \
+ --acl public-read \
  --exclude "$0" \
  "$@"
