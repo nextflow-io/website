dd6b42d0b50870c004a0574f866b50890cf78955
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-07-31 15:12:04 +0200

    Added cache-control directive

--- publish.sh
+++ publish.sh
@@ -4,6 +4,8 @@ AWS_ACCESS_KEY_ID=$NXF_AWS_ACCESS
 AWS_SECRET_ACCESS_KEY=$NXF_AWS_SECRET
  
 aws s3 sync output/ s3://www.nextflow.io \
+ --cache-control max-age=86400 \
+ --metadata-directive REPLACE \
  --storage-class REDUCED_REDUNDANCY \
  --acl public-read \
  --exclude "$0" \
