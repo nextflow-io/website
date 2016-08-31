ea3a0120ec45744206eba1ff1807f6a89ada51fd
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-08-31 10:48:42 +0200

    Updated max-age

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ AWS_ACCESS_KEY_ID=$NXF_AWS_ACCESS
 AWS_SECRET_ACCESS_KEY=$NXF_AWS_SECRET
  
 aws s3 sync output/ s3://www.nextflow.io \
- --cache-control max-age=86400 \
+ --cache-control max-age=2592000 \
  --metadata-directive REPLACE \
  --storage-class REDUCED_REDUNDANCY \
  --acl public-read \
