07a08cb2495222f610ada949921888654e8e4dd0
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-10-20 16:52:22 +0200

    Updated storage class

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ export AWS_SECRET_ACCESS_KEY=$NXF_AWS_SECRET
 aws s3 sync output/ s3://www.nextflow.io \
  --cache-control max-age=2592000 \
  --metadata-directive REPLACE \
- --storage-class REDUCED_REDUNDANCY \
+ --storage-class STANDARD \
  --acl public-read \
  --exclude "$0" \
  "$@"
