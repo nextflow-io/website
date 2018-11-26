9eaa88917336336e7deb986b109d933122c18604
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-11-26 16:29:40 +0100

    Updated www2 hostname

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 export AWS_ACCESS_KEY_ID=$NXF_AWS_ACCESS
 export AWS_SECRET_ACCESS_KEY=$NXF_AWS_SECRET
  
-aws s3 sync output/ s3://www.nextflow.io \
+aws s3 sync output/ s3://www2.nextflow.io \
  --cache-control max-age=2592000 \
  --metadata-directive REPLACE \
  --storage-class STANDARD \
