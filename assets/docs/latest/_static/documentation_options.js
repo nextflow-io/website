9a65d72b1905046c3ec772288d556bfd097f5998
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2020-11-01 19:05:25 +0100

    Bump docs 20.10.0

--- assets/docs/latest/_static/documentation_options.js
+++ assets/docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20.07.1',
+    VERSION: '20.10.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
