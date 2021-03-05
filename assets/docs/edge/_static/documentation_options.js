029897b79bbe00d7e1e3bba32dad1cbc73f5cc1e
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2021-03-05 17:52:20 +0100

    Update docs to 21.03.0-edge

--- assets/docs/edge/_static/documentation_options.js
+++ assets/docs/edge/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '21.02.0-edge',
+    VERSION: '21.03.0-edge',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
