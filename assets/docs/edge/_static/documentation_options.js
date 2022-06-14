979a96ba9c29721761c8063c2f8e9714b3bbfd12
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-06-14 17:54:02 +0200

    Update docs
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- assets/docs/edge/_static/documentation_options.js
+++ assets/docs/edge/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '22.03.1-edge',
+    VERSION: '22.06.0-edge',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
