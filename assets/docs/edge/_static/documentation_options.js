6ca87049aa7dabe62c0f742bbbdaf90ba3d6dbb3
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-07-22 18:27:27 +0200

    Bump 23.07.0-edge docs
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- assets/docs/edge/_static/documentation_options.js
+++ assets/docs/edge/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '23.05.0-edge',
+    VERSION: '23.07.0-edge',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
