2cf3c4ebcd75b1b3162c08df817987be8a5e592e
Phil Ewels <phil.ewels@seqera.io>
2023-08-22 22:49:25 +0200

    Netlify script: Build edge & stable docs, as well as the website

--- netlify_preview_build.sh
+++ netlify_preview_build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#####################################################################
+# NEXTFLOW WEBSITE
+#####################################################################
+
 # DESCRIPTION
 #       for build JBake project on Netlify
 #
@@ -29,3 +33,39 @@ wget --quiet https://github.com/jbake-org/jbake/releases/download/v$jbake_versio
 echo "unzipping JBake v$jbake_version"
 unzip -o -q jbake-$jbake_version-bin.zip
 jbake-$jbake_version/bin/jbake -b
+
+
+#####################################################################
+# NEXTFLOW DOCUMENTATION
+#####################################################################
+
+# Make the empty target directories
+mkdir -p output/docs/latest
+mkdir -p output/docs/edge
+
+# Fetch the Nextflow repo
+cd ../
+gh repo clone nextflow-io/nextflow
+cd nextflow/docs/
+
+# Find the latest stable and edge releases
+STABLE_TAG=$(gh release list --exclude-drafts -L 10 | grep -v edge | head -n 1 | cut -f3)
+EDGE_TAG=$(gh release list --exclude-drafts -L 10 | grep edge | head -n 1 | cut -f3)
+echo "Latest stable release: $STABLE_TAG"
+echo "Latest stable release: $EDGE_TAG"
+
+# Sanity check: Assert string lengths
+if [ ${#STABLE_TAG} -le 4 ]; then echo "Version string too short" ; exit; fi
+if [ ${#EDGE_TAG} -le 4 ]; then echo "Version string too short" ; exit; fi
+
+# Build edge docs
+git checkout $EDGE_TAG
+pip install -r requirements.txt
+make clean html
+mv _build/html/* ../../website/output/docs/edge/
+
+# Build stable docs
+git checkout $STABLE_TAG
+pip install -r requirements.txt
+make clean html
+mv _build/html/* ../../website/output/docs/latest/
