b741e6492022d6d49a9ec027b7e7ec6488bf7fee
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-12-26 16:12:02 +0100

    Bump nextflow docs 23.12.0-edge
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- assets/docs/edge/_static/documentation_options.js
+++ assets/docs/edge/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '23.11.0-edge',
+    VERSION: '23.12.0-edge',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
