f45b76c9e85fc0a0d11934775a35d7bcf074a4fb
Phil Ewels <phil.ewels@seqera.io>
2024-04-05 09:18:29 +0200

    Docs: master->latest, latest->stable (#198)

--- build_docs.sh
+++ build_docs.sh
@@ -9,7 +9,7 @@ WEBSITE_DIR=$(pwd)
 # Make the empty target directories
 mkdir -p output/docs/latest/
 mkdir -p output/docs/edge/
-mkdir -p output/docs/master/
+mkdir -p output/docs/stable/
 
 # Fetch the Nextflow repo
 git clone https://github.com/nextflow-io/nextflow.git
@@ -26,19 +26,22 @@ if [ ${#STABLE_TAG} -le 4 ]; then echo "Version string too short" ; exit 1; fi
 if [ ${#EDGE_TAG} -le 4 ]; then echo "Version string too short" ; exit 1; fi
 
 # Build edge docs
+echo "===============  Building edge docs: $EDGE_TAG  ==============="
 git checkout $EDGE_TAG
 pip install -r requirements.txt
 make clean html
 mv _build/html/* $WEBSITE_DIR/output/docs/edge/
 
 # Build stable docs
+echo "===============  Building stable docs: $STABLE_TAG  ==============="
 git checkout $STABLE_TAG
 pip install -r requirements.txt
 make clean html
-mv _build/html/* $WEBSITE_DIR/output/docs/latest/
+mv _build/html/* $WEBSITE_DIR/output/docs/stable/
 
 # Build current docs on master
+echo "===============  Building latest docs: master  ==============="
 git checkout master
 pip install -r requirements.txt
 make clean html
-mv _build/html/* $WEBSITE_DIR/output/docs/master/
+mv _build/html/* $WEBSITE_DIR/output/docs/latest/
