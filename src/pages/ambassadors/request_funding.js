29d091bcd9098e0c227b86effbd474cc6f5bce70
Marcel Ribeiro-Dantas <mribeirodantas@seqera.io>
2025-01-20 15:24:31 -0300

    Add redirect to ambassador funding request form

--- src/pages/ambassadors/request_funding.js
+++
@@ -1,9 +0,0 @@
-export async function get() {
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: 'https://script.google.com/macros/s/AKfycbwCXBhHS6mE7Zl-PIoRvMplloyaflhLPVLy82nUyYvC-i9x_iAQ2Cdjtr99L0VsPTE/exec',
-    },
-  });
-}
-
