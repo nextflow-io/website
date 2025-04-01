ea998c77407d9ef21b573881ad53c30c5be2e8b2
mavi-sqr <marta.vidal@seqera.io>
2025-04-01 13:06:35 +0200

    Some icons and small fixes

--- src/components/SeqeraHub/index.tsx
+++ src/components/SeqeraHub/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "../Button";
 import Star from "../../../public/img/assets/star.svg";
 import nfcore from "../../../public/img/nf-core.png";
+import PipelinesIcon from "../../../public/img/assets/pipelinesIcon.svg";
+import ContainersIcon from "../../../public/img/assets/containersIcon.svg";
 
 export default function SeqeraHub() {
   const pipelines = [
@@ -47,25 +49,28 @@ export default function SeqeraHub() {
   ];
 
   return (
-    <div className="max-w-7xl mx-auto py-8 px-4">
+    <div className=" mx-auto py-8 px-4">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Pipelines Column */}
         <div>
-          <h2 className="text-2xl font-bold mb-6 md:text-center">Pipelines</h2>
+          <h2 className="text-lg mb-6 md:text-center flex items-center justify-center">
+            <img src={PipelinesIcon.src} alt="Pipelines Icon" className="w-5 h-5 mr-2" />
+            Pipelines
+          </h2>
           <div className="grid grid-cols-1 auto-rows-fr gap-4">
             {pipelines.map((pipeline, index) => (
-              <div key={index} className="border p-6 bg-white shadow-sm h-full">
+              <div key={index} className="border p-6 bg-white shadow-sm h-full min-h-[208px]">
                 <div className="flex flex-col h-full">
                   <div className="flex items-center gap-2">
-                    <h3 className="text-blue-500 font-medium text-lg">{pipeline.name}</h3>
+                    <span className="text-blue-500 font-medium text-lg">{pipeline.name}</span>
                   </div>
 
-                  <p className="text-gray-800 mt-3 mb-auto">{pipeline.description}</p>
+                  <p className=" mt-3 mb-auto">{pipeline.description}</p>
 
                   <div className="mt-4">
                     <div className="flex flex-wrap gap-2 mb-3">
                       {pipeline.tags.map((tag, tagIndex) => (
-                        <span key={tagIndex} className="bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-sm">
+                        <span key={tagIndex} className="bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-xs">
                           {tag}
                         </span>
                       ))}
@@ -98,23 +103,26 @@ export default function SeqeraHub() {
 
         {/* Containers Column */}
         <div>
-          <h2 className="text-2xl font-bold mb-6 md:text-center">Containers</h2>
+          <h2 className="text-lg mb-6 md:text-center flex items-center justify-center">
+            <img src={ContainersIcon.src} alt="Containers Icon" className="w-5 h-5 mr-2" />
+            Containers
+          </h2>
           <div className="grid grid-cols-1 auto-rows-fr gap-4">
             {containers.map((container, index) => (
-              <div key={index} className="border p-6 bg-white shadow-sm h-full">
+              <div key={index} className="border p-6 bg-white shadow-sm h-full  min-h-[208px]">
                 <div className="flex flex-col h-full">
                   <div className="flex items-center gap-2">
-                    <h3 className="text-blue-500 font-medium text-lg flex items-center">
+                    <span className="text-blue-500 font-medium text-lg flex items-center">
                       <span className="mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-orange-500" viewBox="0 0 20 20" fill="currentColor">
                           <path fillRule="evenodd" d="M6 5a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm-2 3a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zm0 3a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zm0 3a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1z" clipRule="evenodd" />
                         </svg>
                       </span>
                       {container.name}
-                    </h3>
+                    </span>
                   </div>
 
-                  <p className="text-gray-800 mt-3 mb-auto">{container.description}</p>
+                  <p className=" mt-3 mb-auto">{container.description}</p>
 
                   <div className="mt-4">
                     <div className="flex items-center text-gray-500 text-sm mb-3">
