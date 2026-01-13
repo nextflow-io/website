df99de4d14e6a70b7b356a383a3649b8adebba48
mavi-sqr <marta.vidal@seqera.io>
2026-01-13 18:19:19 +0100

    Fix sphinx-build PATH issue for Ubuntu Noble 24.04 build image

--- build_docs.sh
+++ build_docs.sh
@@ -16,7 +16,7 @@ git clone https://github.com/nextflow-io/nextflow.git
 cd nextflow/docs/
 
 # Fix sphinx-build path issue for Ubuntu Noble build image
-sed -i 's/SPHINXBUILD ?= sphinx-build/SPHINXBUILD ?= python -m sphinx/' Makefile
+export PATH="$(python -m site --user-base)/bin:$PATH"
 
 # Find the latest stable and edge releases
 STABLE_TAG=$(curl -s https://api.github.com/repos/nextflow-io/nextflow/releases | jq -r ". [].tag_name" | grep -v edge | head -n 1)
