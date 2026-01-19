49438de1464ea9e4b1d74fd777370dcaaec94f85
Georgia Kesisoglou <georgiakes@gmail.com>
2026-01-19 10:29:58 +0100

    Add Georgia Kesisoglou to AmbassadorsList
    Added Georgia Kesisoglou to the ambassadors list with details.

--- src/components/AmbassadorsList/index.tsx
+++ src/components/AmbassadorsList/index.tsx
@@ -333,6 +333,15 @@ const ambassadors: Ambassador[] = [
     twitter: "RikeHanssen",
   },
   {
+    name: "Georgia Kesisoglou",
+    img: "georgiakesisoglou.png",
+    country: "es",
+    github: "georgiakes",
+    linkedin: "geokesisoglou",
+    children:
+      'Georgia is a Bioinformatics Pipeline Developer at CNAG <a href="https://www.cnag.eu/" target="_blank" ></a > for the Variant Calling and Analysis Team <a href="https://www.cnag.eu/teams/bioinformatics-unit/variant-calling-and-analysis" target="_blank" ></a >. She specializes in developing and implementing scalable pipelines for the clinical genomics.', 
+  },
+  {
     name: "Georgie Samaha",
     img: "georgie.jpg",
     country: "au",
